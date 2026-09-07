544fe072daf271e5d9ee967878bda6aa63f283c3
jhonatan-deev <jhonatan-deev@gmail.com>
2026-09-06 23:49:40 -0300

    feat: configura ecommerce-api para service de descoberda do eureka

--- src/main/java/com/jhonatan/ecommerce_api/EcommerceApiApplication.java
+++ src/main/java/com/jhonatan/ecommerce_api/EcommerceApiApplication.java
@@ -2,7 +2,6 @@ package com.jhonatan.ecommerce_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class EcommerceApiApplication {
 
